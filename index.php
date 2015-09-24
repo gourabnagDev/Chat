a9a46bcf6b984417a6670704c9d9a2752b52cfc5
gourabNagDev <gourabnag12@gmail.com>
2015-09-25 00:08:10 +0530

    Version 2.0; Added Emoctions... Just Unicode 🍩 Added UTF-8 character encoding to support unicodes

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <html>
 <head>
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
@@ -86,7 +87,7 @@ else
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title">Modal Header</h4>
+                <h4 class="modal-title">Emoctions</h4>
             </div>
             <div class="modal-body">
                 <p>
